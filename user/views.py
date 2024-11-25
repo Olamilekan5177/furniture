83d43a0f55d87dca4719244e8d5ce76daacbb398
adelalu <adelaluwahab@gmail.com>
2024-11-25 16:32:05 +0100

    update3

--- user/views.py
+++ user/views.py
@@ -3,15 +3,21 @@ from django.shortcuts import render, redirect
 from.forms import RegistrationForm
 from django.contrib.auth.decorators import login_required
 
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from django.contrib.auth import login
+
 def signup(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-           form.save() #save the new user
-           return redirect('login')
+            user = form.save()  # Save the new user
+            login(request, user)  # Log the user in immediately
+            return redirect('profile')  # Redirect to profile or another page after login
     else:
         form = RegistrationForm()
     return render(request, 'user/signup.html', {'form': form})
+
 
 
 
